700a822ceb84e18d6a57f2e7ccd87c16501763ca
Choi Seung In <csissis@naver.com>
2022-02-09 17:00:45 +0900

    Values and Variables
    declare variables using 'let'
    let A = 'B' (variable A's value is B)
    
    standard in JS to write variable names
    - camelCase: when having multiple words in variable, first word in lower case and   next words with upper case
    ex)firstNamePerson
    - using under bar
    ex) first_name_person

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,15 @@
-let js = 'amazing';
-if (js === 'amazing') alert("JavaScript is FUN!");
+let js = "amazing";
+
 console.log(12 + 23 + 61 - 10);
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Bob";
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Student";
+let myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
