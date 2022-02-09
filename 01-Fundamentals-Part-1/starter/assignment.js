4ee215cb7d881608604d006457621bf374a9213b
Choi Seung In <csissis@naver.com>
2022-02-09 19:53:33 +0900

    Assignment - Part 1 : Data Types

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,7 +1,16 @@
 let country = "Korea";
 let continent = "Asia";
-let population = "52.82 million";
+let population = 52.82;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+//Assignment - Part 1 : Values and Variables
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = "true";
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
