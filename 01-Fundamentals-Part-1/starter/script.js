bf765ca5b3d8c4bbdd7b54ae1c97e510a28b7d99
Choi Seung In <csissis@naver.com>
2022-02-09 19:52:47 +0900

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 
 console.log(12 + 23 + 61 - 10);
@@ -10,6 +11,25 @@ console.log(firstName);
 
 let myFirstJob = "Student";
 let myCurrentJob = "Programmer";
-
 console.log(myFirstJob);
 console.log(myCurrentJob);
+*/
+
+console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(year);
+console.log(typeof year);
