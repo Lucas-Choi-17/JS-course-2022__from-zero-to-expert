fba9d89157923066170d6da9fbbdc5a407c3c91f
Choi Seung In <csissis@naver.com>
2022-02-09 20:23:47 +0900

    let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-/*
+/* Values and Variables
 let js = "amazing";
 
 console.log(12 + 23 + 61 - 10);
@@ -15,6 +15,7 @@ console.log(myFirstJob);
 console.log(myCurrentJob);
 */
 
+/* Data Types
 console.log(true);
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -33,3 +34,13 @@ console.log(typeof year);
 year = 1991;
 console.log(year);
 console.log(typeof year);
+*/
+
+let age = 30;
+age = 31;
+console.log(age);
+const birthYear = 1997;
+console.log(birthYear);
+var job = "programmer";
+job = "teacher";
+console.log(job);
