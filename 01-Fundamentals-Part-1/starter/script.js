8d681b4e7242d52aa526522e96a2aea353f9422b
Choi Seung In <csissis@naver.com>
2022-02-10 01:32:00 +0900

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // Math operator
 const now = 2037;
 const ageJonas = now - 1991;
@@ -27,3 +28,20 @@ const isFullAge = ageSarah >= 18;
 console.log(typeof isFullAge);
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+let averageAge = ageJonas + ageSarah / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
