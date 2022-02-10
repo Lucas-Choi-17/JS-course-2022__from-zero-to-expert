cc54e570688c9e54d20844af78b4d10078f0f8d1
Choi Seung In <csissis@naver.com>
2022-02-10 14:22:05 +0900

    Strings and Template Literals
    In JS, we need to use \n\ instead of \n for newline.
    
    Template Literal makes us easy to insert string by using backtick (``)
    And in this case, we can easily make newline by entering Enter

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,47 +1,21 @@
-/*
-// Math operator
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2^3 = 2*2*2
-const firstName = "Lucas";
-const lastName = "Choi";
-console.log(firstName + " " + lastName);
+const firstName = "Jonas";
+const job = "Teacher";
+const birthYear = 1991;
+const year = 2037;
 
-// Assignment operator
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1 = 101
-x--; // x = x - 1 = 100
-x--; // x = x - 1 = 99
-console.log(x);
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(jonas);
 
-// Comparison operator
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
-// <, >, <=, >=
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
 
-const isFullAge = ageSarah >= 18;
-console.log(typeof isFullAge);
+console.log(`Just a regular string...`);
 
-console.log(now - 1991 > now - 2018);
-*/
+console.log("String with \n\
+multiple\n\
+lines");
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-
-console.log(now - 1991 > now - 2018);
-
-let x, y;
-x = y = 25 - 10 - 5; // x = y = 10
-console.log(x, y);
-
-let averageAge = ageJonas + ageSarah / 2;
-console.log(ageJonas, ageSarah, averageAge);
-
-averageAge = (ageJonas + ageSarah) / 2;
-console.log(ageJonas, ageSarah, averageAge);
+console.log(`String with
+multiple
+lines`);
