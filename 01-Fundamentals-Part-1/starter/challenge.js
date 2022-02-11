29958ee240ea34f0545b9301dce9af9091a9ad0c
Choi Seung In <csissis@naver.com>
2022-02-11 12:27:52 +0900

    Coding Challenge #2

--- 01-Fundamentals-Part-1/starter/challenge.js
+++ 01-Fundamentals-Part-1/starter/challenge.js
@@ -1,19 +1,16 @@
 /*
-Mark and John are trying to compare their BMI(Body Mass Index),
- which is calculated using the formula:
-BMI = mass / height ** 2 = mass / (height * height)
- (mass in kg and height in meter).
+Use the BMI example from Challenge #1, 
+and the code you already wrote, 
+and improve it.
+
 Your tasks:
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both
- versions)
-3. Create a Boolean variable 'markHigherBMI' containing information about 
-whether Mark has a higher BMI than John.
-Test data:
-Data 1: Marks weights 78 kg and is 1.69 m tall.
- John weights 92 kg and is 1.95 m tall.
-Data 2: Marks weights 95 kg and is 1.88 m tall.
- John weights 85 kg and is 1.76 m tall.
+1. Print a nice output to the console, 
+saying who has the higher BMI. 
+The message is either "Mark's BMI is higher than John's!" 
+or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. 
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement
 */
 
 let markMass = 78;
@@ -23,17 +20,13 @@ let johnHeight = 1.95;
 
 let markBMI = markMass / markHeight ** 2;
 let johnBMI = johnMass / johnHeight ** 2;
-let markHigherBMI = markBMI > johnBMI;
+let description;
+if (markBMI > johnBMI) {
+  description = `Mark's BMI(${markBMI}) is higher than John's(${johnBMI})`;
+} else if (markBMI < johnBMI) {
+  description = `John's BMI(${johnBMI}) is higher than Mark's(${markBMI})`;
+} else {
+  description = `Mark's and John's BMI is same(${markBMI})`;
+}
 
-console.log(markBMI, johnBMI, markHigherBMI);
-
-markMass = 95;
-markHeight = 1.88;
-johnMass = 85;
-johnHeight = 1.76;
-
-markBMI = markMass / markHeight ** 2;
-johnBMI = johnMass / johnHeight ** 2;
-markHigherBMI = markBMI > johnBMI;
-
-console.log(markBMI, johnBMI, markHigherBMI);
+console.log(description);
