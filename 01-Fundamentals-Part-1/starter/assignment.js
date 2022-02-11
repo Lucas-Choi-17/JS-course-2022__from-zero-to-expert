6e6cf1fdde6312c2d1b8440966441b60e1cd40f9
Choi Seung In <csissis@naver.com>
2022-02-11 11:57:48 +0900

    Assignment - Part 1 : if / else Statement

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -5,5 +5,30 @@ let population = 52.82;
 const isIsland = "true";
 let language = "Korean";
 
-let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+const average = 33;
+
+let description = `${country}'s population is `;
+if (population >= average) {
+  description += `above average.`;
+} else {
+  description += `${average - population} million below average.`;
+}
+console.log(description);
+
+population = 13;
+description = `${country}'s population is `;
+if (population >= average) {
+  description += `above average.`;
+} else {
+  description += `${average - population} million below average.`;
+}
+console.log(description);
+
+population = 130;
+description = `${country}'s population is `;
+if (population >= average) {
+  description += `above average.`;
+} else {
+  description += `${average - population} million below average.`;
+}
 console.log(description);
