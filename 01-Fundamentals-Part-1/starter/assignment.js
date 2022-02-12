ac9c14b73195d6a864409ef8ac42da534afbcaa7
Choi Seung In <csissis@naver.com>
2022-02-12 16:51:17 +0900

    Assignment - Part 1 : Equality Operators: == vs ===

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,10 +1,11 @@
-console.log("9" - "5");
-// 4
-console.log("19" - "13" + "17");
-// 617
-console.log("19" - "13" + 17);
-// 23
-console.log("123" < 57);
-// false
-console.log(5 + 6 + "4" + 9 - 4 - 2);
-// 1143
+const numNeighbors = Number(
+  prompt("How many neighbor countries does your country have?")
+);
+
+if (numNeighbors === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbors > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
