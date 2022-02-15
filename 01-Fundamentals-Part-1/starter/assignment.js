65601d994c994eb958178b1cc1b607a50b0feb69
Choi Seung In <csissis@naver.com>
2022-02-15 16:05:28 +0900

    Assignment - Part 1 : The Conditional (Ternary) Operator

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,22 +1,9 @@
-const language = `Korean`;
+const country = "Korea";
+const population = 52.82;
+const average = 33;
 
-switch (language) {
-  case `Chinese`:
-  case `Mandarin`:
-    console.log(`Most number of native speakers!`);
-    break;
-  case `Spanish`:
-    console.log(`2nd place in number of native speakers`);
-    break;
-  case `English`:
-    console.log(`3rd place`);
-    break;
-  case `Hindi`:
-    console.log(`Number 4`);
-    break;
-  case `Arabic`:
-    console.log(`5th most spoken language`);
-    break;
-  default:
-    console.log(`Great language too :D`);
-}
+console.log(
+  `${country}'s population is ${
+    population > average ? "above" : "below"
+  } average`
+);
