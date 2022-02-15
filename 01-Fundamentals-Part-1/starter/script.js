93e4040a12bb48d9d8b5f93eb12782379eb08d49
Choi Seung In <csissis@naver.com>
2022-02-15 15:58:04 +0900

    The Conditional (Ternary) Operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,24 +1,9 @@
-const day = `thursday`;
+const age = 23;
+age >= 18
+  ? console.log(`I like to drink wine 🍷`)
+  : console.log(`I like to drink water 💧`);
 
-switch (day) {
-  case `monday`: //day === `monday`;
-    console.log(`Plan course structure`);
-    console.log(`Go to coding meetup`);
-    break;
-  case `tuesday`:
-    console.log(`Prepare theory videos`);
-    break;
-  case `wednesday`:
-  case `thursday`:
-    console.log(`Write code examples`);
-    break;
-  case `friday`:
-    console.log(`Record videos`);
-    break;
-  case `saturday`:
-  case `sunday`:
-    console.log(`Enjoy the weekend :D`);
-    break;
-  default:
-    console.log(`Not a valid day!`);
-}
+const drink = age >= 18 ? `wine 🍷` : `water 💧`;
+console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? `wine 🍷` : `water 💧`}`);
