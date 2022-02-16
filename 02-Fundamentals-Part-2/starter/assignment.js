524bb84750db4aa8a4d7fc43b30069c54b55d82b
Choi Seung In <csissis@naver.com>
2022-02-16 12:40:13 +0900

    Assignment - Part 2 : Function  Declarations vs. Expressions

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -1,10 +1,23 @@
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
-}
+"use strict";
 
-const description1 = describeCountry(`Korea`, 52, `Seoul`);
-const description2 = describeCountry(`USA`, 334, `Washington D.C.`);
-const description3 = describeCountry(`India`, 1466, `New Delhi`);
-console.log(description1);
-console.log(description2);
-console.log(description3);
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+const korea1 = percentageOfWorld1(52);
+const usa1 = percentageOfWorld1(348);
+const india1 = percentageOfWorld1(1466);
+
+console.log(`Korea : ${korea1}`);
+console.log(`USA : ${usa1}`);
+console.log(`India : ${india1}`);
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+const korea2 = percentageOfWorld2(52);
+const usa2 = percentageOfWorld2(348);
+const india2 = percentageOfWorld2(1466);
+
+console.log(`Korea : ${korea2}`);
+console.log(`USA : ${usa2}`);
+console.log(`India : ${india2}`);
