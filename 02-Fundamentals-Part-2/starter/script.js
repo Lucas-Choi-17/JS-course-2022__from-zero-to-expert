464d60d0b2a2471e06be1b68ea498cab379e8296
Choi Seung In <csissis@naver.com>
2022-02-21 15:53:01 +0900

    Dot vs. Bracket Notation
    order of properties does not matter because we get properties from object by using property name.
    
    dot notation
    objectName.propertyName
    propertyName should be real final property name
    
    bracket notation
    objectName['propertyName']
    propertyName can also be computed property name

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,9 +1,36 @@
 "use strict";
 
 const lucas = {
-  fristName: "Lucas",
+  firstName: "Lucas",
   lastName: "Choi",
   age: 2022 - 1997,
   job: "student",
   friends: ["Michael", "Peter", "Steven"],
 };
+console.log(lucas);
+
+console.log(lucas.lastName);
+console.log(lucas["lastName"]);
+
+const nameKey = "Name";
+console.log(lucas["first" + nameKey]);
+console.log(lucas["last" + nameKey]);
+
+const interestedIn = prompt(
+  "What po you want to know about Lucas? Choose between firstName, lastName, age, job, and friends"
+);
+
+if (lucas[interestedIn]) {
+  console.log(lucas[interestedIn]);
+} else {
+  console.log(
+    `Wrong request! Choose between firstName, lastName, age, job, and friends`
+  );
+}
+lucas.location = "Korea";
+lucas["twitter"] = "@lucasChoi";
+console.log(lucas);
+
+console.log(
+  `${lucas.firstName} has ${lucas.friends.length} friends, and his best friend is called ${lucas.friends[0]}`
+);
