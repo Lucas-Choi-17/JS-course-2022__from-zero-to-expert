ca0e46ba5e89bce981882c0c9649361eb1171a0d
Choi Seung In <csissis@naver.com>
2022-02-21 14:37:12 +0900

    Coding Challenge #2

--- 02-Fundamentals-Part-2/starter/challenge.js
+++ 02-Fundamentals-Part-2/starter/challenge.js
@@ -1,68 +1,45 @@
 /*
-Back to the two gymnastics teams, the Dolphins and the Koalas!
-There is a new gymnastics discipline, which works differently.
-Each team competes 3 times, and then the average of the 3 scores
-is calculated (so one average score per team).
-A team only wins if it has at least double the average score of
-the other team. Otherwise, no team wins!
+Steven is still building his tip calculator, using the
+same rules as before: Tip 15 % of the bill if the bill
+value is between 50 and 300, and if the value is
+different, the tip is 20 %.
+
 Your tasks:
-1. Create an arrow function 'calcAverage' to calculate the
-average of 3 scores.
-2. Use the function to calculate the average for both teams.
-3. Create a function 'checkWinner' that takes the average score
-of each team as parameters ('avgDolhins' and 'avgKoalas'),
-and then logs the winner to the console, together with the
-victory points, according to the rule above.
-Example: "Koalas win (30 vs. 13)"
-4. Use the 'checkWinner' function to determine the winner for
-both Data1 and Data 2
-5. Ignore draws this time
-Test data:
-Data 1: Dolphins score 44, 23 and 71. 
-Koalas score 65, 54 and 49
-Data 2: Dolphins score 85, 54 and 41.
-Koalas score 23, 34 and 27
-Hints:
-To calculate average of 3 values, add them all together and
-divide by 3
-To check if number A is at least double number B, check for
-A >= 2 * B.
-Apply this to the team's average scores
+1. Write a function 'calcTip' that takes any bill value
+as an input and returns the corresponding tip,
+calculated based on the rules above
+(you can check out the code from first tip calculator
+challenge if you need to). Use the function type you
+like the most. Test the function using a bill value of
+100.
+2. And now let's use arrays! So create an array 'bills'
+containing the test data below.
+3. Create an array 'tips' containing the tip value for
+each bill, calculated from the function you created
+before.
+4. Bonus: Create an array 'total' containing the total
+values, so the bill+tip Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each
+position, and that value can actually be the returned
+value of a function! So you can just call a function as
+array values (so don't store the tip values in separate
+variables first, but right in the new array)
+
+Test data: 125, 555 and 44
 */
 
-// data 1
-// const dolphinsScore1 = 44;
-// const dolphinsScore2 = 23;
-// const dolphinsScore3 = 71;
-
-// const koalasScore1 = 65;
-// const koalasScore2 = 54;
-// const koalasScore3 = 49;
-// data 2
-const dolphinsScore1 = 85;
-const dolphinsScore2 = 54;
-const dolphinsScore3 = 41;
-
-const koalasScore1 = 23;
-const koalasScore2 = 34;
-const koalasScore3 = 27;
-
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-function checkWinner(dolphinsAvg, koalasAvg) {
-  if (dolphinsAvg >= 2 * koalasAvg) {
-    console.log(`Dolphins win (${dolphinsAvg} vs. ${koalasAvg})`);
-  } else if (koalasAvg >= 2 * dolphinsAvg) {
-    console.log(`Koalas win (${koalasAvg} vs. ${dolphinsAvg})`);
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
   } else {
-    console.log(`No team wins...`);
+    return bill * 0.2;
   }
 }
-
-const dolphinsAverage = calcAverage(
-  dolphinsScore1,
-  dolphinsScore2,
-  dolphinsScore3
-);
-const koalasAverage = calcAverage(koalasScore1, koalasScore2, koalasScore3);
-console.log(dolphinsAverage, koalasAverage);
-checkWinner(dolphinsAverage, koalasAverage);
+const bills = new Array(125, 555, 44);
+const tips = [
+  calcTip(bills[0]),
+  calcTip(bills[1]),
+  calcTip(bills[bills.length - 1]),
+];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
