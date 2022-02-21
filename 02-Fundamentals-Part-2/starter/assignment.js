8ddeca69d8d46f68431abc2a58cd1c852f2a9b69
Choi Seung In <csissis@naver.com>
2022-02-21 14:58:02 +0900

    Assignment - Part 2 : Introduction to Objects

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -1,16 +1,9 @@
 "use strict";
 
-const neighbours = new Array(`North Korea`, `China`, `Japan`);
-neighbours.push(`Utopia`);
-console.log(neighbours);
-neighbours.pop();
-console.log(neighbours);
-
-if (!neighbours.includes(`Germany`)) {
-  console.log(`Probably not a central European country :D`);
-}
-
-neighbours[
-  neighbours.indexOf(`North Korea`)
-] = `Democratic People's Republic of Korea`;
-console.log(neighbours);
+const myCountry = {
+  country: "Korea",
+  capital: "Seoul",
+  language: "Korean",
+  population: 52,
+  neighbours: ["North Korea", "Japan", "China"],
+};
