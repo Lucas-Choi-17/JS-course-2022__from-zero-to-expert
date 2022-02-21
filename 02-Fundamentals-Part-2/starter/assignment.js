afac6f8a7cbee4657a2892689304fe64442abd62
Choi Seung In <csissis@naver.com>
2022-02-21 15:59:03 +0900

    Assignment - Part 2 : Dot vs. Bracket Notation

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -7,3 +7,12 @@ const myCountry = {
   population: 52,
   neighbours: ["North Korea", "Japan", "China"],
 };
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+);
+
+myCountry.population += 2;
+console.log(myCountry.population);
+myCountry["population"] -= 2;
+console.log(myCountry.population);
