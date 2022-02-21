fc782c58ae55f3eb70af28005a4deb23a383551d
Choi Seung In <csissis@naver.com>
2022-02-21 12:25:59 +0900

    Introduction to Arrays
    1) const arrayName = [elements....];
    2) const arrayName = new Array(elements...);

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,15 +1,38 @@
-"use strict";
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(years);
 
-  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
-  return juice;
-}
+console.log(friends[0]); // Michael
+console.log(friends[1]); // Steven
+console.log(friends[2]); // Peter
 
-console.log(fruitProcessor(2, 3));
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // last element of friends array
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = `Lucas`;
+const lucas = [firstName, `Choi`, 2022 - 1997, "student", friends];
+console.log(lucas);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2022 - birthYear;
+};
+const years1 = [1990, 1967, 2002, 2010, 2018];
+
+const ages = [
+  calcAge(years1[0]),
+  calcAge(years1[1]),
+  calcAge(years1[2]),
+  calcAge(years1[3]),
+  calcAge(years1[years1.length - 1]),
+];
+console.log(ages);
