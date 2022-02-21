042685a908ebfd1f6e88292716c77df26db27bc5
Choi Seung In <csissis@naver.com>
2022-02-21 14:54:56 +0900

    Introduction to Objects
    const objectName = {
       property1: value, 
       property2: value,
       property3: value,
       ...
    }

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,36 +1,9 @@
 "use strict";
 
-const friends = ["Michael", "Steven", "Peter"];
-// Add elements
-const newLength = friends.push(`jay`);
-console.log(friends);
-console.log(newLength);
-
-const newLength1 = friends.unshift("john");
-console.log(friends);
-console.log(newLength1);
-
-// Remove elements
-const popped = friends.pop();
-console.log(friends);
-console.log(popped);
-
-const popped1 = friends.shift();
-console.log(friends);
-console.log(popped1);
-
-console.log(friends.indexOf("Steven"));
-console.log(friends.indexOf("Bob"));
-
-console.log(friends.includes("Steven"));
-console.log(friends.includes("Bob"));
-
-friends.push(23);
-console.log(friends.includes("23")); // false
-console.log(friends.includes(23)); // true
-console.log(friends.indexOf("23")); // -1
-console.log(friends.indexOf(23)); // 3
-
-if (friends.includes("Steven")) {
-  console.log(`You have friend called Steven`);
-}
+const lucas = {
+  fristName: "Lucas",
+  lastName: "Choi",
+  age: 2022 - 1997,
+  job: "student",
+  friends: ["Michael", "Peter", "Steven"],
+};
