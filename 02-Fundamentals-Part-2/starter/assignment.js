83368b44907479d9c018ceb1a0013c0a61b3bd59
Choi Seung In <csissis@naver.com>
2022-02-22 21:56:05 +0900

    Assignment - Part 2 : Looping Arrays, Breaking and Continuing

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -1,5 +1,13 @@
 "use strict";
 
-for (let voter = 1; voter <= 50; voter++) {
-  console.log(`Voter number ${voter} is currently voting`);
+const populations = [52, 334, 1466, 383];
+const percentages2 = [];
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
 }
+
+for (let i = 0; i <= populations.length - 1; i++) {
+  percentages2.push(percentageOfWorld1(populations[i]));
+}
+console.log(percentages2);
