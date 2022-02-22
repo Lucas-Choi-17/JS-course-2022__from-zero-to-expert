7485a364d8f12a7cc8e06d2d501ee7e864f5f3c5
Choi Seung In <csissis@naver.com>
2022-02-22 21:04:50 +0900

    Iteration: The for Loop
    for loop keeps running while condition is TRUE

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,36 +1,5 @@
 "use strict";
 
-const lucas = {
-  firstName: "Lucas",
-  lastName: "Choi",
-  birthYear: 1997,
-  job: "student",
-  friends: ["Michael", "Peter", "Steven"],
-  hasDriversLicense: true,
-
-  // calcAge: function (birthYear) {
-  //   return 2022 - birthYear;
-  // },
-
-  // calcAge: function () {
-  //   return 2022 - this.birthYear;
-  // },
-
-  calcAge: function () {
-    this.age = 2022 - this.birthYear;
-    return this.age;
-  },
-  // Challenge
-  // "Lucas is a 25-year old student, and he has a/no driver's license"
-  getSummary: function () {
-    return `${this.firstName} is a ${this.age}-year old ${
-      this.job
-    }, and he has ${this.hasDriversLicense ? `a` : `no`} driver's license`;
-  },
-};
-console.log(lucas.calcAge());
-console.log(lucas.age);
-console.log(lucas.age);
-console.log(lucas.age);
-
-console.log(lucas.getSummary());
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`lifting weights repetition ${rep}🏋️‍♀️`);
+}
