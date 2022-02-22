a0bd92fa9411a36e29d6b48f79f09d34baa35d8a
Choi Seung In <csissis@naver.com>
2022-02-22 21:49:36 +0900

    Looping Arrays, Breaking and Continuing
    continue : current iteration of the loop is exited and then the next one starts immediately
    
    break : current iteration of the loop is exited and exit loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,40 @@
 "use strict";
 
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`lifting weights repetition ${rep}🏋️‍♀️`);
+const lucas = [
+  "Lucas",
+  "Choi",
+  2022 - 1997,
+  "student",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+
+for (let i = 0; i < lucas.length; i++) {
+  console.log(lucas[i], typeof lucas[i]);
+
+  // types[i] = typeof lucas[i];
+  types.push(typeof lucas[i]);
+}
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2022 - years[i]);
+}
+console.log(ages);
+
+//continue and break
+console.log(`---- ONLY STRINGS ----`);
+for (let i = 0; i < lucas.length; i++) {
+  if (typeof lucas[i] !== "string") continue;
+  console.log(lucas[i], typeof lucas[i]);
+}
+
+console.log(`---- BREAK WITH NUMBER ----`);
+for (let i = 0; i < lucas.length; i++) {
+  if (typeof lucas[i] === "number") break;
+  console.log(lucas[i], typeof lucas[i]);
 }
