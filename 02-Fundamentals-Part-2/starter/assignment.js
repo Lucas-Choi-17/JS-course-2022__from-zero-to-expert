5268b73582974bfb7e0a8e7cd15db5fbd56c1049
Choi Seung In <csissis@naver.com>
2022-02-22 21:08:34 +0900

    Assignment - Part 2 : Iteration: The for Loop

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -1,21 +1,5 @@
 "use strict";
 
-const myCountry = {
-  country: "Korea",
-  capital: "Seoul",
-  language: "Korean",
-  population: 52,
-  neighbours: ["North Korea", "Japan", "China"],
-
-  describe: function () {
-    console.log(
-      `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`
-    );
-  },
-  checkIsIsland: function () {
-    this.isIsland = this.neighbours.length === 0 ? true : false;
-  },
-};
-myCountry.describe();
-myCountry.checkIsIsland();
-console.log(myCountry.isIsland);
+for (let voter = 1; voter <= 50; voter++) {
+  console.log(`Voter number ${voter} is currently voting`);
+}
