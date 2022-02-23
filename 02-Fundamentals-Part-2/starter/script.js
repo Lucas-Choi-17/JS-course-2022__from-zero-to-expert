72eb8458025ffdd1a2367c9862a0c46cc4cf0122
Choi Seung In <csissis@naver.com>
2022-02-23 11:02:24 +0900

    Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,35 +6,18 @@ const lucas = [
   2022 - 1997,
   "student",
   ["Michael", "Peter", "Steven"],
-  true,
 ];
-const types = [];
 
-for (let i = 0; i < lucas.length; i++) {
-  console.log(lucas[i], typeof lucas[i]);
-
-  // types[i] = typeof lucas[i];
-  types.push(typeof lucas[i]);
-}
-console.log(types);
-
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
-
-for (let i = 0; i < years.length; i++) {
-  ages.push(2022 - years[i]);
-}
-console.log(ages);
-
-//continue and break
-console.log(`---- ONLY STRINGS ----`);
-for (let i = 0; i < lucas.length; i++) {
-  if (typeof lucas[i] !== "string") continue;
-  console.log(lucas[i], typeof lucas[i]);
+// Looping Backwards
+for (let i = lucas.length - 1; i >= 0; i--) {
+  console.log(i, lucas[i]);
 }
 
-console.log(`---- BREAK WITH NUMBER ----`);
-for (let i = 0; i < lucas.length; i++) {
-  if (typeof lucas[i] === "number") break;
-  console.log(lucas[i], typeof lucas[i]);
+// Loops in Loops
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}🏋️‍♀️`);
+  }
 }
