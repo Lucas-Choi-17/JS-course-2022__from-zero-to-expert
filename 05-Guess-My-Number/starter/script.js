bfdc5be38b8b826e0088f94165323770a8dbe294
Choi Seung In <csissis@naver.com>
2022-03-05 15:11:40 +0900

    Manipulating CSS styles
    document.querySelector('selector').style.property = '';
    
    instead of using background-color in css, using backgroundColor in JS

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,11 +6,22 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number!';
-  } else if (guess === secretNumber) {
+  }
+
+  //When player wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-  } else if (guess > secretNumber) {
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+
+  //When guess is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
       score--;
@@ -19,7 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+
+  //When guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
       score--;
